ef7eaf19c0ff5ad7f79d3a06a3d229182459fd59
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-18 12:56:51 +0200

    test

--- tests/integration/components/users-list/component-test.js
+++ tests/integration/components/users-list/component-test.js
@@ -1,15 +1,31 @@
 import { test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import moduleForComponent from 'high-order/tests/helpers/module-for-component';
+import Ember from 'ember';
 
+const { getOwner } = Ember;
 
 moduleForComponent('users-list', 'Integration | Component | users list', {
   integration: true,
+  // beforeEach() {
+  //   let mockedUser = server.create('user', {name: "Andrew"})
+  //   let store = getOwner(this).lookup('service:store');
+  //   let user = store.createRecord('user', mockedUser.attrs);
+  //   return this.set('model', [user])
+  // }
 });
 
+
+
+
 test('it renders', function(assert) {
-  let mockedUser = server.create('user', { name: 'Andrew' })
-  this.set('model', [mockedUser])
+  Ember.run(() => {
+  let mockedUser = server.create('user', {name: "Andrew"})
+    let store = getOwner(this).lookup('service:store');
+    let user = store.createRecord('user', mockedUser.attrs);
+    return this.set('model', [user])
+  })
+
 
   this.render(hbs`{{#users-list users=model as |usr|}}
     {{usr.details.name}}
